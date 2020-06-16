5f7e62049b5e3d892be399f6c21535af5343135b
Declan Mc Candless <declan@screencloud.io>
2020-06-16 12:15:51 +0100

    fix(*): changing install type for publish-package

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const spawn = require("./spawn").spawn;
     await spawn("npm", ["install", "-g", "@screencloud/ops-cli"])
     await spawn("npm", ["install", "-g", "semantic-release"])
     await spawn("npm", ["install", "-g", "@semantic-release/exec"])
-    await spawn("npm", ["install", "-g", "@screencloud/publish-package"])
+    await spawn("npm", ["i", "--save-dev", "@screencloud/publish-package"])
 
 }()
