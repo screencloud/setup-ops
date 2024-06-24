35024634cb149b98fe8b0a363a077612b05386eb
Go Gotaro <me@beam.dev>
2024-06-24 23:35:51 +0700

    feat(*): update setup-ops

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const spawn = require("./spawn").spawn;
 !async function exec() {
 
     console.log("Bootstrapping ScreenCloud Ops Environment")
-    await spawn("npm", ["install", "-g", "@screencloud/ops-cli@1.1.0-beta.15"])
+    await spawn("npm", ["install", "-g", "@screencloud/ops-cli@1.1.0-beta.16"])
     await spawn("npm", ["install", "-g", "semantic-release"])
     await spawn("npm", ["install", "-g", "@semantic-release/exec"])
     await spawn("npm", ["install", "-g", "@screencloud/publish-package"])
